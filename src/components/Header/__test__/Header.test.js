faf1036a21acd2a04ea16e818801979c965ed612
Alvaro-LGPA <alvarolaura@gmail.com>
2023-08-08 18:21:35 +0200

    update

---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,60 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../Header';
+
+
+describe("Header", () => {
+    it('should render same text passed into title prop', () => {
+        render(<Header title="My Header" />);
+        const headingElement = screen.getByText(/My Header/i);
+        expect(headingElement).toBeInTheDocument();
+    });
+
+})
+
+
+/* it('should render same text passed into title prop', () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement).toBeInTheDocument();
+  }); */
+
+/*   it('should render same text passed into title prop', () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByRole("heading", { name:  "My Header"});
+    expect(headingElement).toBeInTheDocument();
+  }); */
+
+
+/*   it('should render same text passed into title prop', () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByTitle("header");
+    expect(headingElement).toBeInTheDocument();
+  });
+ */
+/*   it('should render same text passed into title prop', () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByTestId("header-2");
+    expect(headingElement).toBeInTheDocument();
+  }); */
+
+// Find by
+
+/*   it('should render same text passed into title prop',async () => {
+    render(<Header title="My Header"/>);
+    const headingElement = await screen.findByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+  }); */
+
+  // Query By
+
+/*   it('should render same text passed into title prop',async () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.queryByText(/dogs/i);
+    expect(headingElement).not.toBeInTheDocument();
+  }); */
+
+/*   it('should render same text passed into title prop',async () => {
+    render(<Header title="My Header"/>);
+    const headingElements = screen.getAllByRole('heading');
+    expect(headingElements.length).toBe(2);
+  }); */
